2b59b018385cfd1d54e2e8042bf5e84e2b5e783b
travischase-alianza <travis.chase@alianza.com>
2023-11-29 23:35:37 -0700

    adding day1part1 for testing

---
+++ src/main/java/com/alianza/aoc/day1/Day1Part1.java
@@ -0,0 +1,11 @@
+package com.alianza.aoc.day1;
+
+import com.alianza.aoc.common.ITransform;
+
+public class Day1Part1 implements ITransform {
+    public String transform(String in) {
+
+        return in;
+    }
+    
+}
