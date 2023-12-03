6d8e429c43859472d2c89d160e8dae7a9dfe7d37
travischase-alianza <travis.chase@alianza.com>
2023-12-03 14:25:00 -0700

    added a template for transform and aggregate classes...

---
+++ src/main/java/com/alianza/aoc/day0/Day0Part0Aggregate.java
@@ -0,0 +1,15 @@
+package com.alianza.aoc.day1;
+
+import com.alianza.aoc.common.IAggregate;
+
+public class Day0Part0Aggregate implements IAggregate {
+    int sum = 0;
+
+    public String aggregate(String in) {
+        // TODO: REPLACE TEMPLATE
+
+        System.out.println("[DEBUG Aggregate] New Aggregate: " + in);
+
+        return in;
+    } 
+}
