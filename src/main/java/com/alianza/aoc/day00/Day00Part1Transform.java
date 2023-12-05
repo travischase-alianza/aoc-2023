f7cfeef2ccca377e00a5df8ab30adae6e1e0ae77
travischase-alianza <travis.chase@alianza.com>
2023-12-04 20:58:02 -0700

    fixed template naming

--- src/main/java/com/alianza/aoc/day00/Day00Part1Transform.java
+++ src/main/java/com/alianza/aoc/day00/Day00Part1Transform.java
@@ -2,7 +2,7 @@ package com.alianza.aoc.day00;
 
 import com.alianza.aoc.common.ITransformString;
 
-public class Day0Part1Transform implements ITransformString {
+public class Day00Part1Transform implements ITransformString {
     public String transform(int id, String in) {
          // TODO: REPLACE TEMPLATE
 
