16a6d02d2663a2fcd5c616e1cbb7fd93afdb0789
travischase-alianza <travis.chase@alianza.com>
2023-12-08 22:18:41 -0700

    added report module to each of the remaining days

---
+++ src/main/java/com/alianza/aoc/day06/Day06Part1Report.java
@@ -0,0 +1,11 @@
+package com.alianza.aoc.day06;
+
+import com.alianza.aoc.common.IReport;
+
+public class Day06Part1Report implements IReport {
+    public void report(int lineCount, Object transformResult, Object aggregateResult) {
+        // TODO: REPLACE TEMPLATE
+
+        System.out.println("[DEBUG Report] New Report");
+    } 
+}
---
+++ src/main/java/com/alianza/aoc/day15/Day15Part1Report.java
@@ -0,0 +1,11 @@
+package com.alianza.aoc.day15;
+
+import com.alianza.aoc.common.IReport;
+
+public class Day15Part1Report implements IReport {
+    public void report(int lineCount, Object transformResult, Object aggregateResult) {
+        // TODO: REPLACE TEMPLATE
+
+        System.out.println("[DEBUG Report] New Report");
+    } 
+}
